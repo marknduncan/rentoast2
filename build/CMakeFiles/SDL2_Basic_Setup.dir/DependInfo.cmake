742295d72b7846cd5a614f7d877579f6500759cb
Mark Duncan <m.duncan90@gmail.com>
2019-01-25 23:01:18 -0600

    initial commit

---
+++ build/CMakeFiles/SDL2_Basic_Setup.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/markduncan/Documents/Downloads/SDL2_Basic_Setup-master/src/main.c" "/Users/markduncan/Documents/Downloads/SDL2_Basic_Setup-master/build/CMakeFiles/SDL2_Basic_Setup.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SDL.framework/Headers"
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
